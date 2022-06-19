cdb502967afa6c2820fa131fe4f252877645df1d
Mauricio Olivares <m.olivares.r@gmail.com>
2022-06-19 21:19:17 +0200

    Sincronización, Checkeo cada minuto y Alertas

--- index.py
+++ index.py
@@ -1,21 +1,10 @@
-import json, os, sched, time
-from abc import ABC, abstractmethod
-
-
-# for device in devices: 
-#     device_dir = devices_dir+'/'+device 
-#     with open (device_dir) as file:
-#         data = json.load(file)
-#         if(data["device_state"] == 'normal'):
-#             print(data['device_name'],'Conectado')
-#         else:
-#             print('Alerta! Dispositivo',data['device_name'])
-
+import json, os, time
 class Device: 
-    def __init__(self, id_device, device_name, device_status):
+    def __init__(self, id_device, device_name, device_status, emergency_phone):
         self.__id_device = id_device
         self.__device_name = device_name
         self.__device_status = device_status
+        self.__emergency_phone = emergency_phone
     @property
     def id_device(self):
         return self.__id_device
@@ -26,48 +15,77 @@ class Device:
     @property
     def device_status(self):
         return self.__device_status
+    
+    @property
+    def emergency_phone(self):
+        return self.__emergency_phone
 
 devices_dir = './devices/'
+phones_dir = './phones/'
 
-with os.scandir(devices_dir) as files:
-        files = [device.name for device in files if device.is_file and device.name.endswith('.json')]
+with os.scandir(devices_dir) as devices:
+        devices = [device.name for device in devices if device.is_file and device.name.endswith('.json')]
 
-def check_status(status, device):
-    if(status == 'Conectado'):
-        time.sleep(10)
-        print(device, status)
-    else: 
-        print('Alert', device, status)
+with os.scandir(phones_dir) as phones:
+        phones = [phone.name for phone in phones if phone.is_file and phone.name.endswith('.json')]
 
-def read_json():
-    device_list = []
-    for device in files: 
-        device_dir = devices_dir+'/'+device
-        with open (device_dir) as file:
+def check_status(devices):
+    for device in devices:
+        state = { 'Device': device.device_name, 'Status' : device.device_status, 'Emergency Phone': device.emergency_phone}
+        if(device.device_status != 'Conectado'):
+            print('Alert', state)
+        else: 
+            print(state)
+
+def check_alert(status, device, phone):
+    if(status != 'Conectado'):
+        state = { 'Device': device, 'Status' : status, 'Emergency Phone': phone}
+        print('Alert -', state)
+
+def read_phone(array, dir):
+    phone_list =[]
+    for element in array:
+        inbox = dir+'/'+element
+        with open(inbox) as file:
+            try:
+                data = json.load(file)
+                phone_list.append(data)
+            except:
+                print('No hay teléfono')
+                raise
+    return phone_list
+
+def read_devices(array, dir):
+    object_list = [] 
+    phone_list = read_phone(phones, phones_dir)
+    for element in array:
+        inbox = dir+'/'+element
+        with open (inbox) as file:
             try: 
                 data = json.load(file)
-                device = Device(data['id_device'],data['device_name'],data['device_status'])
-                device_list.append(device)
-                check_status(device.device_status, device.device_name)
-                # print(device_list)
+                valores = data.values()
+                propiedades = list(valores)
+                for phone in phone_list:
+                    if(phone.get('id_phone') == propiedades[3]):
+                        emergency_phone =phone.get('phone')
+                object = Device(propiedades[0],propiedades[1],propiedades[2], emergency_phone)
+                check_alert(object.device_status, object.device_name, object.emergency_phone)
+                object_list.append(object)
             except:
                 print('Dispositivo no tiene información')
                 raise
-    return device_list
+    return object_list
+
 
 def listen_devices(seconds):
-    read_json()
+    read_devices(devices, devices_dir)
     time.sleep(seconds)
 
-
-
-# s = sched.scheduler(time.time, time.sleep)
-
-# def system_state (sc):
-#     print("Dispositivo conectado")
-#     sc.enter(5, 1, system_state,(sc,))
-# s.enter(5,1,system_state, (s,))
-# s.run()
+time_status = 1 
 
 while True:
     listen_devices(1)
+    time_status += 1
+    if(time_status % 60 == 0):
+        device_to_check = read_devices(devices, devices_dir)
+        check_status(device_to_check)
